aa9254db8d875c6d78b0259795728ec9282675cd
macosta2015 <macosta2015@my.fit.edu>
2023-08-01 23:42:27 -0400

    Update App.js

--- email_quotes/src/App.js
+++ email_quotes/src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Email forms: Get multiple quotes from the vendors. Please code
+          Email forms: Get multiple quotes from the vendors. Please code, need to practice
         </p>
       </header>
     </div>
