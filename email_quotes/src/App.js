ebf3382e12b794b7a4f764e94e885c935c7cb7b2
macosta2015 <macosta2015@my.fit.edu>
2023-08-15 23:37:11 -0400

    Update App.js

--- email_quotes/src/App.js
+++ email_quotes/src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          We will be back, we will get in Forbes 30 under 30. LFG
+          We will be back, we will get in Forbes 30 under 30. LFG!
           Email forms: Get multiple quotes from the vendors. Please code, need to practice
         </p>
       </header>
