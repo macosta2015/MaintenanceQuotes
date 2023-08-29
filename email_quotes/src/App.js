4ebf15460291db606f5e9879157603b7b5a3f7fa
macosta2015 <macosta2015@my.fit.edu>
2023-08-28 22:34:08 -0400

    Update App.js

--- email_quotes/src/App.js
+++ email_quotes/src/App.js
@@ -4,7 +4,7 @@ function App() {
       <header className="App-header">
         <p>
           We will be back, we will get in Forbes 30 under 30. LFG! MAKE IT HAPPEN 12 LFG!! Learning to sell
-          Email forms: Get multiple quotes from the vendors. Please code, need to practice
+          Email forms: Get multiple quotes from the vendors. Please code, need to practice. We are building a business. Solid steps for LT!
         </p>
       </header>
     </div>
