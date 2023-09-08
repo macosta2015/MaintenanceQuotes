a63acee8fafee1fbb1968787a939b8710a099279
macosta2015 <macosta2015@my.fit.edu>
2023-09-07 22:14:48 -0400

    Update App.js

--- email_quotes/src/App.js
+++ email_quotes/src/App.js
@@ -3,6 +3,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
+          <h1>
+            Tu mente es capaz de cosas increíbles, pero la tienes que creer.
+          </h1>
           We will be back, we will get in Forbes 30 under 30. LFG! MAKE IT HAPPEN 12 LFG!! Learning to sell
           Email forms: Get multiple quotes from the vendors. Please code, need to practice.   We are building a business. Solid steps for LT12
         </p>
