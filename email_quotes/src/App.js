3a96f66f1c1376bc29bb5c2ffd4cd5b31a1f944f
macosta2015 <macosta2015@my.fit.edu>
2023-09-09 23:10:20 -0400

    commit

--- email_quotes/src/App.js
+++ email_quotes/src/App.js
@@ -7,7 +7,7 @@ function App() {
             Tu mente es capaz de cosas increíbles, pero la tienes que creer.
           </h1>
           <h2>
-            LFG
+            LFG babay
           </h2>
           We will be back, we will get in Forbes 30 under 30. LFG! MAKE IT HAPPEN 12 LFG!! Learning to sell
           Email forms: Get multiple quotes from the vendors. Please code, need to practice.   We are building a business. Solid steps for LT12
